738e3e9198a416de4e34fe26392f3c2100a78c9d
Sandro Montanari <tgdmosaa@shiftux-mac.home>
2017-08-02 21:44:03 +0200

    initial dev

--- spotify.rb
+++ spotify.rb
@@ -1,9 +1,10 @@
 module Spotify
 
   spotifyServer = 'http://192.168.1.125:9000'
+  defaultPlayer = "b8:27:eb:12:7c:b1"
   
   def self.play
-      %x( curl -X GET -H "Content-Type: application/json" -d '{"id":1,"method":"slim.request","params":["b8:27:eb:12:7c:b1",["play"]]}'     http://192.168.1.125:9000/jsonrpc.js )
+      %x( curl -X GET -H "Content-Type: application/json" -d '{"id":1,"method":"slim.request","params":[defaultPlayer,["play"]]}'     spotifyServer/jsonrpc.js )
   end
 
 end
