8d1cc4e2d7e8fb64443a2d239a4775997b255039
Sandro Montanari <tgdmosaa@shiftux-mac.home>
2017-08-03 22:50:51 +0200

    initial dev

--- spotify.rb
+++ spotify.rb
@@ -1,12 +1,30 @@
+require 'faraday'
+
 module Spotify
 
   @@spotifyServer = 'http://192.168.1.125:9000'
   @@defaultPlayer = "b8:27:eb:12:7c:b1"
   
+  # def self.play
+  #   payload = {"id": 1,"method": "slim.request", "params": [@@defaultPlayer, ["play"]]}
+  #   command = "curl -X GET -H \"Content-Type: application/json\" -d "
+  #   hostaddress = "#{@@spotifyServer}/jsonrpc.js"
+  #   %x( "#{command} #{hostaddress}" )
+  #   RestClient.get(hostaddress, payload.to_json, content_type: :json)
+  # end
+
   def self.play
-    puts @@spotifyServer
-    command = 'curl -X GET -H "Content-Type: application/json" -d "{\"id"\:1,\"method\":\"slim.request\",\"params\":[#{@@defaultPlayer},[\"play\"]]}"     "#{@@spotifyServer}"/jsonrpc.js'
-    %x(  )
+    playerCommand = "play"
+    connection = Faraday.new(:url => @@spotifyServer) do |faraday|
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter    # make requests with Net::HTTP
+    end
+
+    response = connection.post do |req|
+        req.url '/jsonrpc.js'
+        req.headers['Content-Type'] = 'application/json'
+        req.body = '{"jsonrpc": "2.0", "id": 1,"method": "slim.request", "params": ["#{@@defaultPlayer}", ["#{playerCommand}"]]}'
+    end
   end
 
 end
