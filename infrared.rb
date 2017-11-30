aa8cbdabdb6f577b876148d5d8d681fb801281da
Sandro Montanari <tgdmosaa@shiftux-mac.home>
2017-11-30 21:44:06 +0100

    modified for broadlink ir blaster

--- infrared.rb
+++ infrared.rb
@@ -1,41 +1,43 @@
+# https://github.com/davorf/BlackBeanControl
+
 module IR
 
   def self.toggle_samsung_power
-      irRequest('samsung', 'KEY_POWER' )
+      irRequest('samsung_power' )
   end
 
   def self.nad_lower_volume
-      irRequest('nad', 'KEY_VOLUMEDOWN' )
-      irRequest('nad', 'KEY_VOLUMEDOWN' )
+      irRequest('nad_volumedown' )
+      irRequest('nad_volumedown' )
   end
 
   def self.nad_increase_volume
-      irRequest('nad', 'KEY_VOLUMEUP' )
-      irRequest('nad', 'KEY_VOLUMEUP' )
+      irRequest('nad_volumeup' )
+      irRequest('nad_volumeup' )
   end
 
   def self.nad_power_off
-      irRequest('nad', 'KEY_POWER2' )
+      irRequest('nad_poweroff' )
   end
 
   def self.nad_power_on
-      irRequest('nad', 'KEY_POWER' )
+      irRequest('nad_poweron' )
   end
 
   def self.arcam_music_input
-      irRequest('arcam', 'KEY_NUMERIC_1' )
+      irRequest('arcam_musicinput' )
   end
 
   def self.arcam_tv_input
-      irRequest('arcam', 'KEY_NUMERIC_2' )
+      irRequest('arcam_tvinput' )
   end
 
   def self.samsung_toggle_power
-      irRequest('samsung', 'KEY_POWER' )
+      irRequest('samsung_power' )
   end
 
-  def self.irRequest(device, key)
-    command = "irsend SEND_ONCE #{device} #{key}"
+  def self.irRequest(command)
+    command = "python BlackBeanControl.py -c #{command}"
     %x( eval "#{command}" )
   end
 
---
+++ infrared.rb.lirc.rb
@@ -0,0 +1,42 @@
+module IR
+
+  def self.toggle_samsung_power
+      irRequest('samsung', 'KEY_POWER' )
+  end
+
+  def self.nad_lower_volume
+      irRequest('nad', 'KEY_VOLUMEDOWN' )
+      irRequest('nad', 'KEY_VOLUMEDOWN' )
+  end
+
+  def self.nad_increase_volume
+      irRequest('nad', 'KEY_VOLUMEUP' )
+      irRequest('nad', 'KEY_VOLUMEUP' )
+  end
+
+  def self.nad_power_off
+      irRequest('nad', 'KEY_POWER2' )
+  end
+
+  def self.nad_power_on
+      irRequest('nad', 'KEY_POWER' )
+  end
+
+  def self.arcam_music_input
+      irRequest('arcam', 'KEY_NUMERIC_1' )
+  end
+
+  def self.arcam_tv_input
+      irRequest('arcam', 'KEY_NUMERIC_2' )
+  end
+
+  def self.samsung_toggle_power
+      irRequest('samsung', 'KEY_POWER' )
+  end
+
+  def self.irRequest(device, key)
+    command = "irsend SEND_ONCE #{device} #{key}"
+    %x( eval "#{command}" )
+  end
+
+end
