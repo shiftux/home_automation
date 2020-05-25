5f89ee6402d48aa8e0cf496958b06230a6113365
Sandro Montanari <sandro@inpher.io>
2020-05-25 11:41:30 +0200

    attribute try

--- home-assistant/config/www/tileboard/pages/transportPage.js
+++ home-assistant/config/www/tileboard/pages/transportPage.js
@@ -4,15 +4,15 @@ const transportPage = {
   icon: 'mdi-train',
   groups: [
     {
-      title: '',
-      width: 3,
-      height: 4,
+      title: 'Galgenfeld -> Bern Bahnhof (9 min)',
+      width: 2,
+      height: 3,
       items: [
         {
           position: [0, 0],
           width: 2,
           type: TYPES.SENSOR,
-          title: 'Bern Bahnhof',
+          title: '',
           id: 'sensor.next_departure',
           unit: '', // override default entity unit
           state: false, // hidding state
@@ -26,7 +26,26 @@ const transportPage = {
             }
             return time;
           }        
-        }
+        },
+        {
+          position: [0, 1],
+          width: 2,
+          type: TYPES.SENSOR,
+          title: '',
+          id: 'sensor.next_departure',
+          unit: '', // override default entity unit
+          state: false, // hidding state
+          filter: function (value) { // optional
+            const num = new Date(Date.parse(@attributes.next_departure));
+            var time = num.getHours() + ":"
+            if (num.getMinutes().toString().length > 1) {
+              time = time + num.getMinutes()
+            } else {
+              time = time + "0" + num.getMinutes()
+            }
+            return time;
+          }        
+        },
       ]
     }
   ]
